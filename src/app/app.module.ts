2592ed19461814c47064a8c42c1e81bafe4c8ee8
Alex Fedotov <alfumit@gmail.com>
2018-07-22 23:24:58 +0300

    Continuing down the tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,16 @@ import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import {HeroesComponent} from './heroes/heroes.component';
+
 import {TransferHttpCacheModule} from '@nguniversal/common';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule.withServerTransition({appId: 'my-app'}),
